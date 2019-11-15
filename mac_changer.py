18c3b68af0b44878e872ecb02e658821c7affbd8
Nirolfix <florinstamate@gmail.com>
2019-11-15 11:41:58 +0100

    regex2

--- mac_changer.py
+++ mac_changer.py
@@ -2,17 +2,23 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
-    parser.add_option('-i', '--interface', dest='interface', help='Interface to change the MAC address')
-    parser.add_option('-m', '--mac', dest='new_mac', help='The New MAC address')
+    parser.add_option('-i', '--interface', dest='interface',
+                      help='Interface to change the MAC address')
+    parser.add_option('-m', '--mac', dest='new_mac',
+                      help='The New MAC address')
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        parser.error('[-] Please specify an interface, use --help for more info.')
+        parser.error(
+            '[-] Please specify an interface, use --help for more info.')
     elif not options.new_mac:
-        parser.error('[-] Please specify a new_mac value, use --help for more info.')
+        parser.error(
+            '[-] Please specify a new_mac value, use --help for more info.')
     return options
+
 
 def change_mac(interface, new_mac):
     print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
@@ -24,5 +30,9 @@ def change_mac(interface, new_mac):
 options = get_arguments()
 # change_mac(options.interface, options.new_mac)
 
-ifconfig_result = subprocess.check_output(['ifconfig', options.interface])
-print(ifconfig_result)
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+# print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+print(mac_address_search_result.group(0))
