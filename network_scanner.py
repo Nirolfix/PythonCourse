c9aa47beb4e57918b6f9bc5e371ec25cf09b3500
Nirolfix <florinstamate@gmail.com>
2019-11-19 22:17:28 +0100

    netscan version 1

---
+++ network_scanner.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+import scapy.all as scapy
+
+
+def scan(ip):
+    # scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
+    # scapy.ls(scapy.Ether())
+
+
+scan('192.168.9.171')
